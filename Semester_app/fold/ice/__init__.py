3c53440c0b9039dd263a5ffc88c6bd790830cbb0
richardmyrdal <126146440+richardmyrdal@users.noreply.github.com>
2023-03-05 16:06:30 +0100

    Delete Semester_app directory

--- Semester_app/fold/ice/__init__.py
+++
@@ -1,2 +0,0 @@
-#
-#Dette fungerer som en pakke i python, som gjør at vi kan lage Blueprint
--- Semester_app/fold/meth/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template, Blueprint
-
-meth=Blueprint('meth',__name__)
-
-#NB! Jeg har forkortet methodology til meth
-@meth.route('/meth')
-def meth_home():
-  return render_template('meth.html', title='meth')
